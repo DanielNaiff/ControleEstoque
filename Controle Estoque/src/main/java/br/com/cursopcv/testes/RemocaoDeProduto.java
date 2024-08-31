4b43a3d8221da286e6f58d09272b48c24b4eb2ee
DanielNaiff <danielnaiff344@gmail.com>
2024-08-31 17:07:00 -0300

    passos: "No pacote “testes” crie uma classe chamada “RemocaoDeProduto” com o método “main” para executarmos alguns comandos;  Encapsule uma conexão para todas as chamadas de acesso ao banco, utilizando o Hibernate;  Inicie a transação, chame o método “find” do “EntityManager”, passando a classe e o código do terceiro produto (3). Chame o método “remove” do “EntityManager”;  Comita e fecha a conexão. Veja no MySQLWorkbech se a exclusão aconteceu."

--- Controle Estoque/src/main/java/br/com/cursopcv/testes/RemocaoDeProduto.java
+++ Controle Estoque/src/main/java/br/com/cursopcv/testes/RemocaoDeProduto.java
@@ -1,7 +1,38 @@
 package br.com.cursopcv.testes;
 
+import br.com.cursopcv.modelo.Produto;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 public class RemocaoDeProduto {
     public static void main(String[] args) {
-        
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("ControleEstoque");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction transaction = null;
+
+        try {
+            transaction = em.getTransaction();
+            transaction.begin();
+            Produto produto = em.find(Produto.class, 3L);
+            if (produto != null) {
+                em.remove(produto);
+                System.out.println("Produto removido com sucesso!");
+            } else {
+                System.out.println("Produdo com id 3 nao encontrado");
+            }
+            transaction.commit();
+        }catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+
+            em.close();
+            emf.close();
+        }
     }
 }
