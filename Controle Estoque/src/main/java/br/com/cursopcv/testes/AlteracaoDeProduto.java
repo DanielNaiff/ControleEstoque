454316cb8372a2fc594e5ff102f8f7d9f6982f49
DanielNaiff <danielnaiff344@gmail.com>
2024-08-31 16:58:57 -0300

    passo: "Inicie a transação, chame o método “find” do “EntityManager”, passando a classe e o código do segundo produto (2). Altere o preço dele de R$ 450.68 para R$ 345.00.  Comita e fecha a conexão. Veja no MySQLWorkbech se a alteração aconteceu."

--- Controle Estoque/src/main/java/br/com/cursopcv/testes/AlteracaoDeProduto.java
+++ Controle Estoque/src/main/java/br/com/cursopcv/testes/AlteracaoDeProduto.java
@@ -1,4 +1,40 @@
 package br.com.cursopcv.testes;
 
+import br.com.cursopcv.modelo.Produto;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 public class AlteracaoDeProduto {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("ControleEstoque");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction transaction = null;
+
+        try {
+            transaction = em.getTransaction();
+            transaction.begin();
+
+            Produto produto = em.find(Produto.class, 2L); // O ID do produto é 2
+            if (produto != null) {
+                produto.setPreco(345.00);
+                em.merge(produto);
+            }
+
+            transaction.commit();
+            System.out.println("Produto alterado com sucesso!");
+
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            em.close();
+            emf.close();
+        }
+    }
 }
