e9b9948f5c32bca0fe252300fbe63ae8bec6c211
DanielNaiff <danielnaiff344@gmail.com>
2024-08-31 10:57:35 -0300

    passo: "Na classe “Produto” incluir os atributos e seus getter’s e setter’s:"

---
+++ Controle Estoque/src/main/java/br/com/cursopcv/modelo/Produto.java
@@ -0,0 +1,39 @@
+package br.com.cursopcv.modelo;
+
+public class Produto {
+    public String nome;
+    public String descricao;
+    public double preco;
+
+    public Produto(String nome, String descricao, double preco) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+    }
+
+    public Produto(){}
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+}
