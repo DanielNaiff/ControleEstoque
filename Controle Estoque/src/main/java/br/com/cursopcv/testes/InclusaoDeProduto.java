248eb47e1ff2b20a101ac39faeb5ac8aa5049c03
DanielNaiff <danielnaiff344@gmail.com>
2024-08-31 12:10:44 -0300

    passo: "Encapsule uma conexão para todas as chamadas ao banco de dados, utilizando o Hibernate;”Panela  Crie o objeto “prop” da classe Produto e faça o cadastro dos produtos:  “Panela” - “Panela de Pressão 10 Litros” - 56.80;  “Cama” - “Cama de casal Big” – 450.68;  “Caixa Som” – “Caixa de Som JBL” – 160.00;"

--- Controle Estoque/src/main/java/br/com/cursopcv/testes/InclusaoDeProduto.java
+++ Controle Estoque/src/main/java/br/com/cursopcv/testes/InclusaoDeProduto.java
@@ -1,7 +1,43 @@
 package br.com.cursopcv.testes;
 
+import br.com.cursopcv.modelo.Produto;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 public class InclusaoDeProduto {
+
     public static void main(String[] args) {
-        
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("ControleEstoque");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction transaction = null;
+
+        try {
+            transaction = em.getTransaction();
+            transaction.begin();
+
+            Produto panela = new Produto("Panela", "Panela de Pressão 10 Litros", 56.80);
+            Produto cama = new Produto("Cama", "Cama de casal Big", 450.68);
+            Produto caixaSom = new Produto("Caixa Som", "Caixa de Som JBL", 160.00);
+
+            em.persist(panela);
+            em.persist(cama);
+            em.persist(caixaSom);
+
+            transaction.commit();
+
+            System.out.println("Produtos cadastrados com sucesso!");
+
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            em.close();
+            emf.close();
+        }
     }
 }
